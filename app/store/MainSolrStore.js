8e08f76285adb94e10efe8038eaaf0a2262f36fa
timoatkudotedu <timoatkudotedu@ce9bf4bf-376f-1848-a496-a1c9fe3b1a39>
2012-09-10 23:04:47 +0000

    mostly mapping stuff
    git-svn-id: http://svn.code.sf.net/p/specify/code/trunk/SpecifyEnterprise/SpecifyWeb/PortalApp@10587 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- app/store/MainSolrStore.js
+++ app/store/MainSolrStore.js
@@ -26,6 +26,7 @@ Ext.define('SpWebPortal.store.MainSolrStore', {
 	geoCoordFlds: [], //due to mysterious initialization behavior, this
 	                 //is not set until the MainGrid.js initComponent() is executed
 	maxPageSize: typeof maxPageSizeSetting === "undefined" ? 5000 : maxPageSizeSetting,
+	geoCoordVals: [] 
     },
 
     autoLoad: false,
@@ -41,6 +42,40 @@ Ext.define('SpWebPortal.store.MainSolrStore', {
 	    root: 'response.docs',
 	    totalProperty: 'response.numFound'
 	}
+    },
+
+    getLatLngFitFilter: function(lat, lng, sw, ne) {
+	var result = lat + ':[' + sw.lat() + ' TO ' + ne.lat() + ']+AND+';
+	if (sw.lng() > 0 && ne.lng() < 0) {
+	    result += 'NOT ' + lng + ':[' + ne.lng() + ' TO ' + sw.lng() + ']';
+	} else if (sw.lng() > 0 && ne.lng() > 0 && sw.lng() > ne.lng()) {
+	    result += lng + ':[' + ne.lng() + ' TO ' + sw.lng() + ']';
+	} else {
+	    result +=  lng + ':[' + sw.lng() + ' TO ' + ne.lng() + ']';
+	}
+	return result;
+    },
+
+    getMapFitFilter: function() {
+	var map = Ext.getCmp('spwpmainmappane').getMapCmp();
+	var bnds = map.getBounds();
+	var sw = bnds.getSouthWest();
+	var ne = bnds.getNorthEast();
+
+	var lat1Fld = this.getGeoCoordFlds()[0];
+	var lng1Fld = this.getGeoCoordFlds()[1];
+	var lat2Fld = this.getGeoCoordFlds().length > 2 ? this.getGeoCoordFlds()[2] : null;
+	var lng2Fld = this.getGeoCoordFlds().length > 3 ? this.getGeoCoordFlds()[3] : null;
+
+	var result = '';
+	if (lat1Fld != null && lng1Fld != null) {
+	    result += this.getLatLngFitFilter(lat1Fld, lng1Fld, sw, ne);
+	}
+	if (lat2Fld != null && lng2Fld != null) {
+	    result += this.getLatLngFitFilter(lat2Fld, lng2Fld, sw, ne)
+	}
+	//console.info(result);
+	return result;
     },
 
     changePageSize: function(newPageSize) {
