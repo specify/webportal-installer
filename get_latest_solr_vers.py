2656c693ea6eb799c4ff3a988fe411e97a10ab1c
Ben Anhalt <anhalt@ku.edu>
2014-03-28 11:32:31 -0500

    pin solr version to 4.7.*

--- get_latest_solr_vers.py
+++ get_latest_solr_vers.py
@@ -3,10 +3,10 @@ import re
 
 index_html = sys.stdin.read()
 
-matches = re.findall(r'href="4\.6\.(\d+)/"', index_html)
+matches = re.findall(r'href="4\.7\.(\d+)/"', index_html)
 
 versions = [tuple(int(x) for x in v) for v in matches]
 
 latest = sorted(versions, reverse=True)[0]
 
-print '4.6.%d' % latest
+print '4.7.%d' % latest
