f668fc984dd12f5d5104d0fb294afa7129cda0ff
timo <timo@ku.edu>
2019-07-03 16:45:28 -0500

    don't add cross-origin filter if it's already there

--- patch_web_xml.py
+++ patch_web_xml.py
@@ -29,25 +29,37 @@ def add_init_param(element, name, value):
 
 webxml = etree.parse(sys.argv[1])
 root = webxml.getroot()
-crfilter = etree.SubElement(root, 'filter')
-fname = etree.SubElement(crfilter, 'filter-name')
-fname.text = 'cross-origin'
-fclass = etree.SubElement(crfilter, 'filter-class')
-fclass.text = 'org.eclipse.jetty.servlets.CrossOriginFilter'
-add_init_param(crfilter, 'allowedOrigins','http://localhost')
-add_init_param(crfilter, 'allowedMethods', 'GET,POST,DELETE,PUT,HEAD,OPTIONS')
-add_init_param(crfilter, 'allowedHeaders','origin, content-type, cache-control, accept, options, authorization, x-requested-with')
-add_init_param(crfilter, 'supportsCredentials','true')
-add_init_param(crfilter, 'chainPreflight','false')
 
-crmapping = etree.SubElement(root, 'filter-mapping')
-fname = etree.SubElement(crmapping, 'filter-name')
-fname.text = 'cross-origin'
-pattern = etree.SubElement(crmapping, 'url-pattern')
-pattern.text = '/*'
+addcrossorigin = true
+for c in root.getchildren():
+    if c.tag.endswith('filter'):
+        for fn in c.getchildren():
+            if fn.tag.endswith('filter-name') and fn.text == 'cross-origin':
+                addcrossorigin = False
+                break
+    if not addcrossorigin:
+        break
 
-root.insert(0, crfilter)
-root.insert(1, crmapping)
+if addcrossorigin:
+    crfilter = etree.SubElement(root, 'filter')
+    fname = etree.SubElement(crfilter, 'filter-name')
+    fname.text = 'cross-origin'
+    fclass = etree.SubElement(crfilter, 'filter-class')
+    fclass.text = 'org.eclipse.jetty.servlets.CrossOriginFilter'
+    add_init_param(crfilter, 'allowedOrigins','http://localhost')
+    add_init_param(crfilter, 'allowedMethods', 'GET,POST,DELETE,PUT,HEAD,OPTIONS')
+    add_init_param(crfilter, 'allowedHeaders','origin, content-type, cache-control, accept, options, authorization, x-requested-with')
+    add_init_param(crfilter, 'supportsCredentials','true')
+    add_init_param(crfilter, 'chainPreflight','false')
+
+    crmapping = etree.SubElement(root, 'filter-mapping')
+    fname = etree.SubElement(crmapping, 'filter-name')
+    fname.text = 'cross-origin'
+    pattern = etree.SubElement(crmapping, 'url-pattern')
+    pattern.text = '/*'
+    
+    root.insert(0, crfilter)
+    root.insert(1, crmapping)
 
 # Done.
 
