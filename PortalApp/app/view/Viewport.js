3cad5646e246acf917b6baf31ca33b767c8b7464
Maksym Patiiuk <info@mambo.in.ua>
2020-01-31 16:34:59 -0600

    Fix #42

--- PortalApp/app/view/Viewport.js
+++ PortalApp/app/view/Viewport.js
@@ -1,18 +1,18 @@
 /* Copyright (C) 2018, University of Kansas Center for Research
- * 
+ *
  * Specify Software Project, specify@ku.edu, Biodiversity Institute,
  * 1345 Jayhawk Boulevard, Lawrence, Kansas, 66045, USA
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -35,7 +35,7 @@ Ext.define('SpWebPortal.view.Viewport', {
     mapsCheckBoxTip: 'Check to select only records with geo-coordinates',
     imagesCheckBox: 'Images',
     imagesCheckBoxTip: 'Check to select only records with images',
-    fitToMapCheckBox:'Fit to Map',
+    fitToMapCheckBox:'Search Locally',
     fitToMapCheckBoxTip: 'Check to apply search criteria within map region',
     settingsBtnTip: 'Settings',
     mapSearchBtn: 'Search',
@@ -114,21 +114,21 @@ Ext.define('SpWebPortal.view.Viewport', {
 
 	mapBtn.setVisible(false);
 	mapCancelBtn.setVisible(false);
-	
+
 	var settings = Ext.getStore('SettingsStore').getAt(0);
 	var attUrl = settings.get("imageBaseUrl");
 	var attachmentsPresent = typeof attUrl === "string" && attUrl.length > 0;
         var expOK = settings.get("allowExportToFile");
-        
+
         if (typeof expOK === "undefined" || expOK != 0) {
             expOK = true;
         } else {
             expOK = false;
         }
-        
+
 	var expBtn = Ext.create('Ext.button.Button', {
 	    xtype: 'button',
-	    tooltip: this.toCsv, 
+	    tooltip: this.toCsv,
 	    icon: 'resources/images/ExportExcelTemplate16x16.png',
 	    itemid: 'spwpexpcsvbtn',
 	    id: 'spwpexpcsvbtn',
